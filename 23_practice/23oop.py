c015d7d222b3c250348a9ea5519f7f4f8a2b3599
nermakov2903-lab <nermakov2903@gmail.com>
2025-11-09 23:22:37 +0700

    Add files via upload

---
+++ 23_practice/23oop.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+
+class Iproduct(ABC):      # базовый интерфейс, для всех продуктов
+    @abstractmethod
+    def release(self):
+        pass
+
+
+class Car(Iproduct):   # конкретный продукт
+    def release(self):
+        print("Выпущен новый легковой автомобиль")
+
+
+class Truck(Iproduct):   # конкретный продукт
+    def release(self):
+        print("Выпущен новый грузовой автомобиль")
+
+
+class IworkShop:    # Это интерфейс для всех "мастерских"
+    @abstractmethod
+    def create(self) -> Iproduct:
+        pass
+
+
+class CarWorkShop(IworkShop):   # фабрика 1
+    def create(self):
+        return Car()
+
+
+class TruckWorkShop(IworkShop):  # фабрика 2
+    def create(self):
+        return Truck()
+
+
+if __name__ == "__main__":
+    creator = CarWorkShop()
+    car = creator.create()
+
+    creator = TruckWorkShop()
+    truck = creator.create()
+
+    car.release()
+    truck.release()
+
+
